176a0c97063f0b36e6331a73ea7423aa26dd2312
alphegasolutions <alphegasolutions@gmail.com>
2022-05-05 13:41:23 -0400

    updated environment variables

--- pkg/vars/vars.go
+++ pkg/vars/vars.go
@@ -4,13 +4,22 @@ import (
 	"github.com/zbitech/common/interfaces"
 	"github.com/zbitech/common/pkg/model/config"
 	"github.com/zbitech/common/pkg/utils"
+	"strconv"
 )
 
 var (
-	MONGODB_URL          = utils.GetEnv("MONGODB_URL", "mongodb://root:wzZxK2YCCn@db.zbitech.local:27017")
-	MONGODB_NAME         = utils.GetEnv("MONGODB_NAME", "zbiRepo")
+	DATABASE_FACTORY     = utils.GetEnv("DATABASE_FACTORY", "mongo")
+	DATABASE_URL         = utils.GetEnv("DATABASE_URL", "mongodb://root:wzZxK2YCCn@db.zbitech.local:27017")
+	DATABASE_NAME        = utils.GetEnv("DATABASE_NAME", "zbiRepo")
 	ASSET_PATH_DIRECTORY = utils.GetEnv("ASSET_PATH_DIRECTORY", "tests/files/etc/zbi")
-	HOURS_IN_YEAR        = 8760
+	KUBECONFIG           = utils.GetEnv("KUBECONFIG", "cfg/kubeconfig")
+	K8S_INCLUSTER, _     = strconv.ParseBool(utils.GetEnv("K8SINCLUSTER", "true"))
+	EXPIRATION_HOURS, _  = strconv.Atoi(utils.GetEnv("EXPIRATION_HOURS", "8760"))
+	ADMIN_USER           = utils.GetEnv("ZBI_ADMIN_USER", "admin")
+	ADMIN_EMAIL          = utils.GetEnv("ZBI_ADMIN_EMAIL", "admin@alphegasolutions.com")
+	ADMIN_PASSWORD       = utils.GetEnv("ZBI_ADMIN_PASSWORD", "password")
+
+	HOURS_IN_YEAR = 8760
 
 	KlientFactory        interfaces.KlientFactoryIF
 	RepositoryFactory    interfaces.RepositoryFactoryIF
