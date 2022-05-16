91528d0a007de67f22cc187c8fb4d44efba1f58a
alphegasolutions <alphegasolutions@gmail.com>
2022-05-15 22:05:40 -0400

    added team to project request

--- pkg/model/object/project_request.go
+++ pkg/model/object/project_request.go
@@ -8,5 +8,6 @@ type ProjectRequest struct {
 	Network     ztypes.NetworkType `json:"network" validate:"required"`
 	Version     string             `json:"version" validate:"required"`
 	Name        string             `json:"name" validate:"required"`
+	Team        string             `json:"team"`
 	Description string             `json:"description" validate:"required"`
 }
