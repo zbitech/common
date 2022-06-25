21bb5fe8935197a4a1eaa4a026184210b6471b4e
alphegasolutions <alphegasolutions@gmail.com>
2022-06-25 00:03:12 -0400

    refactored jwt functionality

--- pkg/jwtutil/jwtutil.go
+++ pkg/jwtutil/jwtutil.go
@@ -2,7 +2,9 @@ package jwtutil
 
 import (
 	"context"
+	"github.com/zbitech/common/pkg/id"
 	"github.com/zbitech/common/pkg/model/entity"
+	"github.com/zbitech/common/pkg/model/object"
 	"github.com/zbitech/common/pkg/rctx"
 	"strings"
 	"time"
@@ -11,6 +13,10 @@ import (
 	"github.com/zbitech/common/pkg/errs"
 	"github.com/zbitech/common/pkg/logger"
 	"github.com/zbitech/common/pkg/vars"
+)
+
+var (
+//	SECRET_KEY = "KaPdSgVkYp3s6v9y$B&E)H@McQeThWmZ"
 )
 
 func ValidateAuthorizationToken(ctx context.Context, authorization string) (jwt.Claims, *entity.User, error) {
@@ -43,5 +49,43 @@ func ValidateAuthorizationToken(ctx context.Context, authorization string) (jwt.
 	}
 
 	return claims, user, nil
+}
 
+func GenerateJwtToken(user entity.User) (*string, error) {
+
+	now := time.Now()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, object.ZBIBasicClaims{
+		StandardClaims: jwt.StandardClaims{
+			Audience:  "ZBI",
+			ExpiresAt: now.Add(time.Hour * time.Duration(vars.AppConfig.Policy.TokenExpirationPolicy)).Unix(),
+			Id:        id.GenerateRequestID(),
+			IssuedAt:  now.Unix(),
+			Issuer:    "ZBI",
+			NotBefore: now.Unix(),
+			Subject:   user.UserId,
+		},
+		Role:  user.Role,
+		Email: user.Email,
+	})
+
+	signedToken, err := token.SignedString([]byte(vars.AppConfig.Repository.JwtConfig.SecretKey))
+	if err != nil {
+		return nil, err
+	}
+
+	return &signedToken, nil
+}
+
+func ParseWithClaims(tokenString string, claims jwt.Claims, validateToken func(token *jwt.Token) error) (*jwt.Token, error) {
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		err := validateToken(token)
+		if err != nil {
+			return nil, err
+		}
+
+		return []byte(vars.AppConfig.Repository.JwtConfig.SecretKey), nil
+	})
+
+	return token, err
 }
